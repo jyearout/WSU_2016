1432bd030da4907dbaf190dde73f0a62cd91b9fd
smumford <smumfo@gmail.com>
2016-06-26 16:13:48 -0500

    this is my prisoner file

--- 1.3.9-10 sourceFiles/team5.py
+++ 1.3.9-10 sourceFiles/team5.py
@@ -9,24 +9,27 @@
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
-    
+
+import random
+      
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
     '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if 'b' in their_history[-2:]: # If the other player has betrayed within last 2 rounds, 
+        return 'b'               # Betray.
+    else:
+        if random.random()<0.15: # 15% of the other rounds
+            return 'b'         # Betray
+        else:
+            return 'c'         # but 85% of the time collude
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
